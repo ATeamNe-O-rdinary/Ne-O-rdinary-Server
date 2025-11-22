cd724c7280a6d3c3d16adbb778788e7af03550f1
박지훈 <jihun6548@naver.com>
2025-11-22 16:41:12 +0900

    feat: trigger 테스트

--- src/test/java/org/ateam/ateam/AteamApplicationTests.java
+++ src/test/java/org/ateam/ateam/AteamApplicationTests.java
@@ -3,7 +3,7 @@ package org.ateam.ateam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //test
 class AteamApplicationTests {
 
 	@Test
