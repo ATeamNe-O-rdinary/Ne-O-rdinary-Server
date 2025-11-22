553ad7311808ace77e240fa3c2f910bdc8099393
박지훈 <jihun6548@naver.com>
2025-11-22 22:47:28 +0900

    feat: Swagger응답 형식 ErrorCodeOperationCustom 변경

--- src/main/java/org/ateam/ateam/global/s3/S3Controller.java
+++ src/main/java/org/ateam/ateam/global/s3/S3Controller.java
@@ -1,16 +1,14 @@
 package org.ateam.ateam.global.s3;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.ateam.ateam.global.config.swagger.ApiErrorCodeExamples;
 import org.ateam.ateam.global.dto.ResponseDto;
+import org.ateam.ateam.global.error.ErrorCode;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import io.swagger.v3.oas.annotations.Operation;
 
-@Tag(name = "S3 파일", description = "S3 파일 업로드 / 삭제 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/files")
@@ -18,22 +16,37 @@ public class S3Controller {
 
   private final S3Uploader s3Uploader;
 
-  @Operation(summary = "파일 업로드", description = "S3에 파일을 업로드합니다.")
-  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<ResponseDto<S3UploadResponse>> upload(
+  @Operation(summary = "파일 업로드")
+  @ApiErrorCodeExamples({
+      ErrorCode.S3_UPLOAD_FAILED,
+      ErrorCode.S3_INVALID_URL
+  })
+  @PostMapping
+  public ResponseDto<S3UploadResponse> upload(
       @RequestPart("file") MultipartFile file,
-      @RequestParam(defaultValue = "default") String dirName) {
+      @RequestParam(defaultValue = "default") String dirName
+  ) {
     String url = s3Uploader.upload(file, dirName);
-    S3UploadResponse response = new S3UploadResponse(url);
-
-    return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK, null, "파일 업로드 성공", response));
+    return ResponseDto.of(
+        HttpStatus.OK,
+        "S3_UPLOAD_SUCCESS",
+        "파일 업로드 성공",
+        new S3UploadResponse(url)
+    );
   }
 
-  @Operation(summary = "파일 삭제", description = "S3에 업로드된 파일을 삭제합니다.")
+  @Operation(summary = "파일 삭제")
+  @ApiErrorCodeExamples({
+      ErrorCode.S3_DELETE_FAILED,
+      ErrorCode.S3_INVALID_URL
+  })
   @DeleteMapping
-  public ResponseEntity<ResponseDto<Void>> delete(@RequestParam String url) {
+  public ResponseDto<Void> delete(@RequestParam String url) {
     s3Uploader.delete(url);
-
-    return ResponseEntity.ok(ResponseDto.<Void>of(HttpStatus.OK, null, "파일 삭제 성공"));
+    return ResponseDto.of(
+        HttpStatus.NO_CONTENT,
+        "S3_DELETE_SUCCESS",
+        "파일 삭제 성공"
+    );
   }
 }
